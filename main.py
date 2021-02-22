007b1c990c3a4994b61ff4a17a8fe276d05f36f6
DmytroHoretskyi <serwserw16@gmail.com>
2021-02-22 15:16:32 +0300

    added lab1

---
+++ main.py
@@ -0,0 +1,11 @@
+from cycle import *
+
+if __name__ == "__main__":
+    firstCycle = Cycle(1, 40, 120, "Jarik", 14, "blue", False)
+    secondCycle = Cycle(24, 20, 100, "Desna",  14, "blue",)
+    thirdCycle = Cycle(16, 60, 220, "Ukraina")
+
+    print("first cycle: ", firstCycle.__str__())
+    print("second cycle: ", secondCycle.__str__())
+    print("thidr cycle: ", thirdCycle.__str__())
+
